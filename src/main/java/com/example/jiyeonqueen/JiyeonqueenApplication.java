afbc278c7ef7b72c0c9e40031e8b2b36c1738cc3
Leehaneuli <rosehee0916@naver.com>
2025-02-25 23:04:31 +0900

    start

---
+++ src/main/java/com/example/jiyeonqueen/JiyeonqueenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiyeonqueen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiyeonqueenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiyeonqueenApplication.class, args);
+	}
+
+}
