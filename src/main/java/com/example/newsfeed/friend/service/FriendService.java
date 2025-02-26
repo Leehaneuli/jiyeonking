e4072c7c6d232473b52ca39e20f1cf984828171c
jiyeon0926 <yeoni9094@naver.com>
2025-02-27 02:43:54 +0900

    fix: 친구 신청 기능 수정
    - 이미 거절된 데이터가 존재하면 기존 데이터는 삭제하고 새로 데이터 저장
    - 더미 데이터 방지

--- src/main/java/com/example/newsfeed/friend/service/FriendService.java
+++ src/main/java/com/example/newsfeed/friend/service/FriendService.java
@@ -9,8 +9,10 @@ import com.example.newsfeed.friend.repository.FriendRepository;
 import com.example.newsfeed.user.entity.User;
 import com.example.newsfeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +29,16 @@ public class FriendService {
     public FriendResponseDto sendFriend(Long followerId, Long followeeId) {
         Optional<User> follower = userRepository.findById(followerId);
         Optional<User> followee = userRepository.findById(followeeId);
+
+        Optional<Friend> optionalFriend = friendRepository.findByFollowerIdAndFolloweeId(followerId, followeeId);
+
+        if (optionalFriend.isPresent()) {
+            if (optionalFriend.get().getStatus() == FriendStatus.REJECTED) {
+                friendRepository.delete(optionalFriend.get());
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 친구 신청을 보냈습니다.");
+            }
+        }
 
         Friend friend = new Friend(follower.get(), followee.get());
         Friend savedFriend = friendRepository.save(friend);
