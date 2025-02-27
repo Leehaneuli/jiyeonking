59b391c2194df33ab965b2b82b2382c4942ae113
jiyeon0926 <yeoni9094@naver.com>
2025-02-27 18:52:47 +0900

    feat: 댓글 전체 조회

--- src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -2,10 +2,13 @@ package com.example.newsfeed.comment.repository;
 
 import com.example.newsfeed.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +19,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     }
 
     Optional<Comment> findCommentByBoardIdAndId(Long boardId, Long commentId);
+
+    @Query("SELECT c FROM Comment c INNER JOIN User u ON c.user.id = u.id WHERE c.isDeleted = false AND c.board.id = :boardId")
+    List<Comment> findCommentsByBoardId(@Param("boardId") Long boardId);
 }
